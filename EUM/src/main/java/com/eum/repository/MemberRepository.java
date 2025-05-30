0caa5f96feba5b29e50c4d7ca30cd4cc79c6d224
fraidy53 <152137109+fraidy53@users.noreply.github.com>
2025-05-30 16:35:13 +0900

    Update MemberRepository.java

--- EUM/src/main/java/com/eum/repository/MemberRepository.java
+++ EUM/src/main/java/com/eum/repository/MemberRepository.java
@@ -3,5 +3,9 @@ package com.eum.repository;
 import com.eum.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 // 데이터베이스 접근 인터페이스
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByUsernameAndNameAndEmail(String username, String name, String email);
+    Optional<Member> findByUsername(String username);
 } 
