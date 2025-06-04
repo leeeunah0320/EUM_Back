3f6cc2a3e100831b0a21e5f61b3488517f9a3f36
fraidy53 <152137109+fraidy53@users.noreply.github.com>
2025-06-04 21:28:03 +0900

    Update MemberRepository.java

--- EUM/src/main/java/com/eum/repository/MemberRepository.java
+++ EUM/src/main/java/com/eum/repository/MemberRepository.java
@@ -9,4 +9,5 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByUsernameAndNameAndEmail(String username, String name, String email);
     Optional<Member> findByUsername(String username);
     Optional<Member> findByNameAndEmail(String name, String email);
+    Optional<Member> findByEmail(String email);
 } 
