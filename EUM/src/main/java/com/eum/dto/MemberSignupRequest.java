026ba8e6832d7b1d80256e62c8d39498d7a9ecc3
fraidy53 <152137109+fraidy53@users.noreply.github.com>
2025-06-23 20:43:29 +0900

    Update MemberSignupRequest.java

--- EUM/src/main/java/com/eum/dto/MemberSignupRequest.java
+++ EUM/src/main/java/com/eum/dto/MemberSignupRequest.java
@@ -7,7 +7,7 @@ import lombok.Setter;
 @Setter
 // 회원가입요청 데이터 객체체
 public class MemberSignupRequest {
-    private String memberId;
+    private String username;
     private String password;
     private String name;
     private String email;
