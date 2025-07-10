90882aff49494fc0dfec7fc460e08a8296265c9d
fraidy53 <152137109+fraidy53@users.noreply.github.com>
2025-07-10 17:13:46 +0900

    Update SecurityConfig.java

--- EUM/src/main/java/com/eum/config/SecurityConfig.java
+++ EUM/src/main/java/com/eum/config/SecurityConfig.java
@@ -16,7 +16,7 @@ public class SecurityConfig {
         http
             .csrf(AbstractHttpConfigurer::disable)
             .authorizeHttpRequests(auth -> auth
-            .requestMatchers("/", "/login", "/oauth2/**", "/error", "/api/auth/**", "/api/members/**", "/rekognition/test").permitAll()
+            .requestMatchers("/", "/login", "/oauth2/**", "/error", "/api/auth/**", "/api/members/**", "/api/ocr/**", "/rekognition/test", "/ocr.html").permitAll()
             .anyRequest().authenticated()
             )
             .oauth2Login(oauth2 -> oauth2
