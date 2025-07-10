d40c9c3d4ed57dbbf21fbd7e46c8721b1af3fc14
fraidy53 <152137109+fraidy53@users.noreply.github.com>
2025-07-10 17:09:07 +0900

    Create AwsConfig.java

---
+++ EUM/src/main/java/com/eum/config/AwsConfig.java
@@ -0,0 +1,51 @@
+package com.eum.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.rekognition.RekognitionClient;
+import software.amazon.awssdk.services.translate.TranslateClient;
+import software.amazon.awssdk.services.polly.PollyClient;
+
+@Configuration
+public class AwsConfig {
+
+    @Value("${aws.access-key-id}")
+    private String accessKeyId;
+
+    @Value("${aws.secret-access-key}")
+    private String secretAccessKey;
+
+    @Value("${aws.region}")
+    private String region;
+
+    @Bean
+    public RekognitionClient rekognitionClient() {
+        return RekognitionClient.builder()
+                .credentialsProvider(StaticCredentialsProvider.create(
+                        AwsBasicCredentials.create(accessKeyId, secretAccessKey)))
+                .region(Region.of(region))
+                .build();
+    }
+
+    @Bean
+    public TranslateClient translateClient() {
+        return TranslateClient.builder()
+                .credentialsProvider(StaticCredentialsProvider.create(
+                        AwsBasicCredentials.create(accessKeyId, secretAccessKey)))
+                .region(Region.of(region))
+                .build();
+    }
+
+    @Bean
+    public PollyClient pollyClient() {
+        return PollyClient.builder()
+                .credentialsProvider(StaticCredentialsProvider.create(
+                        AwsBasicCredentials.create(accessKeyId, secretAccessKey)))
+                .region(Region.of(region))
+                .build();
+    }
+} 
